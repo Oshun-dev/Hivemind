4b6ec1c004d879ea97cb4f5bf241a1ff149b43ff
William <william.vanhaelst@etudiant.univ-rennes1.fr>
2024-08-31 00:09:36 +0200

    test

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-alert("SOYEZ TÉMOINS");
+// ==UserScript==
+// @name         Hivemind
+// @namespace    http://hivemind.com/
+// @version      0.1.0
+// @description  Data analyzer for fourmizzz
+// @author       William Van Haelst
+// @match        http://s4.fourmizzz.fr/
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Votre code ici...
+    alert('SOYEZ TÉMOINS');
+})();
