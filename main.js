3c1f567ea5db6f5243a889bcc7a172017b632720
William <william.vanhaelst@etudiant.univ-rennes1.fr>
2024-09-01 01:00:01 +0200

    test gapi

--- main.js
+++ main.js
@@ -8,9 +8,26 @@
 // @grant        none
 // ==/UserScript==
 
-(function() {
+(async function() {
     'use strict';
 
-    // Votre code ici...
-    alert('SOYEZ TÉMOINS');
+    const SPREADSHEET_ID = '1912KUGBGzpPKM3t0QpHQJyde0kYK4Qtc1xgX3cc35d8'
+    const CLIENT_ID = ''
+    const API_KEY = 'AIzaSyCXmK-Auhzwj6XOclM1yViv-3lA4-hJiAc'
+    const SCOPE = 'https://www.googleapis.com/auth/spreadsheets'
+
+    const url = `${SCOPE}/${SPREADSHEET_ID}/values/Sheet1!A1:D5?key=${API_KEY}`;
+    
+    try {
+        // Envoyer la requête GET à l'API Google Sheets.
+        const response = await fetch(url);
+        if (!response.ok) throw new Error('Erreur de requête : ' + response.status);
+
+        // Convertir la réponse en JSON.
+        const data = await response.json();
+
+        console.log('Données récupérées depuis Google Sheets :', data);
+    } catch (error) {
+        console.error('Erreur lors de la récupération des données :', error);
+    }
 })();
